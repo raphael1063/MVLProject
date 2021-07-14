19f8734b4824b237159ed7839d98e6e73c2f9a43
wjlim2 <sysoftwjlim2@gmail.com>
2021-07-14 18:01:40 +0900

    Feat(NetworkModule.kt) : 네트워크 모듈 추가

---
+++ app/src/main/java/com/robin/mvlproject/di/NetworkModule.kt
@@ -0,0 +1,13 @@
+package com.robin.mvlproject.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+}
