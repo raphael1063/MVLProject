8d620e0a947e4d09dc5e13278f9f925214b6e2b8
wonjinlim <raphael1050302@gmail.com>
2021-07-14 23:44:33 +0900

    Feat : 홈 지도화면 작업 - GoogleMap 적용

--- app/src/main/java/com/robin/mvlproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/robin/mvlproject/ui/home/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.robin.mvlproject.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.robin.mvlproject.R
 import com.robin.mvlproject.base.BaseFragment
 import com.robin.mvlproject.databinding.FragmentHomeBinding
@@ -12,18 +17,46 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     R.layout.fragment_home
 ), OnMapReadyCallback {
     override fun start() {
-        childFragmentManager.beginTransaction()
-            .add(R.id.mapView, SupportMapFragment.newInstance())
-            .commit()
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+
+                } else {
+                    requireActivity().finish()
+                }
+            }
+
+        val mapFragment = parentFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                childFragmentManager.beginTransaction()
+                    .add(R.id.mapView, SupportMapFragment.newInstance())
+                    .commit()
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
+
     }
 
     override fun observe() {
     }
 
-    companion object {
-        val instance = HomeFragment()
+    override fun onMapReady(map: GoogleMap) {
     }
 
-    override fun onMapReady(map: GoogleMap) {
+    companion object {
+        val instance = HomeFragment()
     }
 }
