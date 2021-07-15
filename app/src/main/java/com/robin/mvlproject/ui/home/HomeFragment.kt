240e63406d0e6abfe8e6f8df892a4fea4bf688a3
wjlim2 <sysoftwjlim2@gmail.com>
2021-07-15 11:49:34 +0900

    Feat(fragment_home.xml) : 지도 중앙 위치의 위도 경도 찾기 구현

--- app/src/main/java/com/robin/mvlproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/robin/mvlproject/ui/home/HomeFragment.kt
@@ -2,22 +2,27 @@ package com.robin.mvlproject.ui.home
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.robin.mvlproject.R
 import com.robin.mvlproject.base.BaseFragment
 import com.robin.mvlproject.databinding.FragmentHomeBinding
+import timber.log.Timber
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(
     R.layout.fragment_home
-), OnMapReadyCallback {
-    override fun start() {
+), OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
 
+    private var map: GoogleMap? = null
+
+    override fun start() {
+        Timber.d("start!!!!!!!!!!!")
         val requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
@@ -29,17 +34,15 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
                 }
             }
 
-        val mapFragment = parentFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
 
-        when {
+
+        when (PackageManager.PERMISSION_GRANTED) {
             ContextCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                childFragmentManager.beginTransaction()
-                    .add(R.id.mapView, SupportMapFragment.newInstance())
-                    .commit()
+            ) -> {
+                val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+                mapFragment?.getMapAsync(this@HomeFragment)
             }
             else -> {
                 requestPermissionLauncher.launch(
@@ -54,9 +57,19 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     }
 
     override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        val seoul = LatLng(37.56, 126.97)
+        map.addMarker(MarkerOptions().position(seoul).title("서울"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10F))
+        map.setOnCameraMoveListener(this)
     }
 
     companion object {
         val instance = HomeFragment()
+    }
+
+    override fun onCameraMove() {
+        Timber.d("CameraPosition = ${map?.cameraPosition}")
+//        binding.tvHomeLabelA.text = "lat = ${map?.cameraPosition?.target?.latitude} \n lng = ${map?.cameraPosition?.target?.longitude}"
     }
 }
