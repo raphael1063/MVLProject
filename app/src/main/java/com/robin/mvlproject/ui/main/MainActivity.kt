cd80ee83b311ebfe5a598236f2adfae1bd8ba3dc
wjlim2 <sysoftwjlim2@gmail.com>
2021-07-16 12:36:36 +0900

    Feat : MainActivity 에서 각 화면으로 이동하는 로직 적용

--- app/src/main/java/com/robin/mvlproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/robin/mvlproject/ui/main/MainActivity.kt
@@ -1,23 +1,57 @@
 package com.robin.mvlproject.ui.main
 
-import android.os.Bundle
+import androidx.activity.viewModels
 import com.robin.mvlproject.R
 import com.robin.mvlproject.base.BaseActivity
+import com.robin.mvlproject.data.entities.Step.*
 import com.robin.mvlproject.databinding.ActivityMainBinding
+import com.robin.mvlproject.ui.detail.DetailFragment
+import com.robin.mvlproject.ui.history.HistoryFragment
 import com.robin.mvlproject.ui.home.HomeFragment
+import com.robin.mvlproject.ui.price.PriceFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
 ) {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun start() {
         supportFragmentManager.beginTransaction()
             .add(R.id.fcv_container, HomeFragment.instance)
+            .addToBackStack(null)
             .commit()
     }
 
     override fun observe() {
+        with(viewModel) {
+            openDetail.observe(this@MainActivity, { event ->
+                event.getContentIfNotHandled()?.let { label ->
+                    supportFragmentManager.beginTransaction()
+                        .add(R.id.fcv_container, DetailFragment.getInstance(label))
+                        .addToBackStack(null)
+                        .commit()
+                }
+            })
+            openPrice.observe(this@MainActivity, { event ->
+                event.getContentIfNotHandled()?.let { label ->
+                    supportFragmentManager.beginTransaction()
+                        .add(R.id.fcv_container, PriceFragment.instance)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            })
+            openHistory.observe(this@MainActivity, { event ->
+                event.getContentIfNotHandled()?.let {
+                    supportFragmentManager.beginTransaction()
+                        .add(R.id.fcv_container, HistoryFragment.instance)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            })
+        }
     }
 
 }
--- app/src/main/java/com/robin/mvlproject/ui/main/MainViewModel.kt
+++ app/src/main/java/com/robin/mvlproject/ui/main/MainViewModel.kt
@@ -1,14 +1,40 @@
 package com.robin.mvlproject.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import com.robin.mvlproject.Event
 import com.robin.mvlproject.base.BaseViewModel
 import com.robin.mvlproject.data.Repository
+import com.robin.mvlproject.data.entities.Label
+import com.robin.mvlproject.data.entities.Step
+import com.robin.mvlproject.data.entities.Step.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle,
     private val repository: Repository
 ) : BaseViewModel() {
+
+    private val _openDetail = MutableLiveData<Event<Label>>()
+    val openDetail: LiveData<Event<Label>> = _openDetail
+
+    private val _openPrice = MutableLiveData<Event<List<Label>>>()
+    val openPrice: LiveData<Event<List<Label>>> = _openPrice
+
+    private val _openHistory = MutableLiveData<Event<Unit>>()
+    val openHistory: LiveData<Event<Unit>> = _openHistory
+
+    fun openDetail(label: Label) {
+        _openDetail.value = Event(label)
+    }
+
+    fun openPrice(labelList: List<Label>) {
+        _openPrice.value = Event(labelList)
+    }
+
+    fun openHistory() {
+        _openHistory.value = Event(Unit)
+    }
 }
