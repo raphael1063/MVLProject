5cd9fcb986a66b6fa5f52920c8358ddfa7d62bcb
wjlim2 <sysoftwjlim2@gmail.com>
2021-07-16 12:38:39 +0900

    Feat : 레이블 상세화면 작업

--- app/src/main/java/com/robin/mvlproject/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/robin/mvlproject/ui/detail/DetailFragment.kt
@@ -1,15 +1,37 @@
 package com.robin.mvlproject.ui.detail
 
+import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.robin.mvlproject.R
 import com.robin.mvlproject.base.BaseFragment
+import com.robin.mvlproject.data.entities.Label
 import com.robin.mvlproject.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>(
     R.layout.fragment_detail
 ) {
+
+    private val viewModel: DetailViewModel by viewModels()
+
     override fun start() {
+        binding.vm = viewModel
+        requireArguments().getParcelable<Label>("label")?.let { label ->
+            viewModel.loadData(label)
+        }
     }
 
     override fun observe() {
+    }
+
+    companion object {
+        fun getInstance(label: Label): DetailFragment {
+            return DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable("label", label)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/robin/mvlproject/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/robin/mvlproject/ui/detail/DetailViewModel.kt
@@ -1,14 +1,33 @@
 package com.robin.mvlproject.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.robin.mvlproject.data.Repository
+import com.robin.mvlproject.data.entities.Label
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle,
     private val repository: Repository
 ) : ViewModel() {
+
+    private val _label = MutableLiveData<Label>()
+    val label: LiveData<Label> = _label
+
+    var nickname = MutableLiveData<String>()
+
+    fun loadData(label: Label) {
+        _label.value = label
+        _label.value?.let {
+            nickname.value = it.nickname ?: ""
+        }
+
+    }
+
+    fun onSaveButtonClicked() {
+
+    }
 }
