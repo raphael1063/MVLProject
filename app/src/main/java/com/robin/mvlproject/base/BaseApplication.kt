8d7120a22bbe154df4dee35ba619859a35656c09
wonjinlim <raphael1050302@gmail.com>
2021-07-19 23:30:10 +0900

    Refactor : Application 클래스 코드 정리

--- app/src/main/java/com/robin/mvlproject/base/BaseApplication.kt
+++ app/src/main/java/com/robin/mvlproject/base/BaseApplication.kt
@@ -9,17 +9,6 @@ class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
         Timber.plant(Timber.DebugTree())
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-        instance = null
-    }
-
-    companion object {
-        @Volatile
-        private var instance: BaseApplication? = null
     }
 }
