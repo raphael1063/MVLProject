d61ecb278012d58b014afc0b59def0f70e30d59d
wjlim2 <sysoftwjlim2@gmail.com>
2021-07-19 17:47:27 +0900

    Feat : Label Entity 수정

--- app/src/main/java/com/robin/mvlproject/data/entities/Label.kt
+++ app/src/main/java/com/robin/mvlproject/data/entities/Label.kt
@@ -1,17 +1,20 @@
 package com.robin.mvlproject.data.entities
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
+import com.robin.mvlproject.data.Converters
 import com.robin.mvlproject.data.entities.LabelType.*
-import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = "label")
 data class Label(
-    var type: LabelType,
+
+    @PrimaryKey(autoGenerate = true)
+    val idx: Long,
+
+    @ColumnInfo(name = "labelType")
+    var type: LabelType?,
 
     @ColumnInfo(name = "locationInfo")
     var locationInfo: String,
@@ -28,30 +31,5 @@ data class Label(
     @ColumnInfo(name = "name")
     var name: String?
 ) : Parcelable {
-
-    constructor(name: LabelType, locationInfo: String, aqi: Int, nickname: String?) : this(
-        name,
-        locationInfo,
-        0.0,
-        0.0,
-        aqi,
-        nickname
-    )
-
-    constructor(aqi: Int, lat: Double, lng: Double, name: String?) : this(
-        UNDEFINED,
-        "",
-        lat,
-        lng,
-        aqi,
-        name
-    )
-
-    @IgnoredOnParcel
-    @PrimaryKey(autoGenerate = true)
-    val idx: Long = 0L
-}
-
-enum class LabelType {
-    A, B, UNDEFINED
+    constructor(aqi: Int, latitude: Double, longitude: Double, locationInfo: String) : this(0, UNDEFINED,  locationInfo, latitude, longitude, aqi, null)
 }
